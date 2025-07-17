e86063e6be254402347a1c0ad65c2011ee5020d1
Adham Saeed <adhamsaeeed0500@gmail.com>
2025-07-17 15:08:31 +0300

    feat(getAllProducts): add getAllProductsWithSuppliers Endpoint

--- src/controllers/productcontroller.js
+++ src/controllers/productcontroller.js
@@ -2,10 +2,12 @@ const express = require('express');
 const { productSchema } = require('../middlewares/validator');
 const supplierModel = require('../models/suppliermodel');
 const Product = require('../models/productmodel');
+const { get } = require('mongoose');
+const productmodel = require('../models/productmodel');
 
 
 
-exports.addProducts = async(req , res) => {
+exports.addProducts= async(req , res) => {
     const {name , code,  category , price , quantity , supplier } = req.body;
 
     const {error  ,value } = productSchema.validate({name , category , price , quantity , supplier });
@@ -43,3 +45,28 @@ exports.addProducts = async(req , res) => {
         return res.status(400).json({success:false, message:'there is an error please try again' , error:error.message})
     }
 }
+
+exports.getAllProducts = async(req , res) => {
+    try {
+        const products = await productmodel
+            .find({})
+            .select('name category price quantity')
+            .populate('supplier', 'name phone')
+            .lean(); 
+
+
+        return res.status(200).json({
+            success: true,
+            result: products
+        });
+
+        
+    } catch (error) {
+        return res.status(400).json({
+            success: false,
+            message: 'An error occured during trying to retrieve products',
+            error: error.message
+        });
+
+    }
+};
--- src/routers/productRouter.js
+++ src/routers/productRouter.js
@@ -3,6 +3,7 @@ const productController = require('../controllers/productcontroller');
 const router = express.Router();
 
  router.post('/', productController.addProducts);
+ router.get('/', productController.getAllProducts);
 
 
  module.exports = router;
