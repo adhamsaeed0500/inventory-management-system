84ea853ff7ebfa933f8487a2d12b1ba5d2e6109b
Adham Saeed <adhamsaeeed0500@gmail.com>
2025-07-18 12:45:41 +0300

    add : add notificationRouter

---
+++ src/routers/notificationRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const notificationcontroller = require('../controllers/notificationcontroller');
+const router = express.Router();
+
+
+ router.get('/', notificationcontroller.getAllNotifications);
+ router.delete('/:id',notificationcontroller.deleteNotification);
+ router.put('/:id', notificationcontroller.updateStatus);
+
+
+ module.exports = router;
