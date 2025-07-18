aba2fe6df75bc304b15e4981f44f9fb705725e8a
Adham Saeed <adhamsaeeed0500@gmail.com>
2025-07-18 21:25:03 +0300

    add : Sale ModelSchema to DB

---
+++ src/models/salemodel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const saleSchema = new mongoose.Schema({
+  customerName: String,
+  products: [{
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+    quantity: Number,
+    unitPrice: Number
+  }],
+  totalPrice: Number,
+  date: { type: Date, default: Date.now },
+  invoiceNumber: String
+});
+
+module.exports = mongoose.model('Sale', purchaseSchema);
